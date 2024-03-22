673cc3c0284ca18de45e7e1f05818a95fd7cc278
Vishwas Sharma <ch0nky@protonmail.com>
2024-03-22 17:48:30 +0530

    Initial Commit

---
+++ BBQUOTES/BBQUOTESApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQUOTESApp.swift
+//  BBQUOTES
+//
+//  Created by Vishwas Sharma on 22/03/24.
+//
+
+import SwiftUI
+
+@main
+struct BBQUOTESApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
